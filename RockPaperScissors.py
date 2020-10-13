5281fcb9c4d2f84b6621ba5e75ad82ec341c556d
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-10-13 21:19:21 +0330

    create user_select(), system_select() def

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,4 +1,28 @@
-def play():
-    print("yes")
+from config import RULES
+from random import choice
 
-play()
+
+def user_select():
+    user_input = ''
+    user_input = input("insert r,p,s... : ")
+    if user_input not in RULES:
+        print("Oops...")
+        user_select()
+    else:
+        return user_input
+
+
+def system_select():
+    system_select = choice(RULES)
+    return system_select
+
+
+def play(user, system):
+    print(user, system)
+
+
+user_select = user_select()
+system_select = system_select()
+
+
+play(user_select, system_select)
