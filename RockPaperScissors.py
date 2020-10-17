e817ffca4361d01bacdb9d5c5d49e9a66fc74255
Alireza Kariminejad <alireza.kariminejad@yahoo.com>
2020-10-18 01:45:48 +0330

    update who_win def

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,6 +1,8 @@
 from config import RULES, RESULT
 from random import choice
 
+result = {"user": 0, "system": 0}
+
 
 def user_select():
     user_input = ''
@@ -13,24 +15,31 @@ def user_select():
 
 
 def system_select():
-    system_select = choice(RULES)
-    return system_select
+    system = choice(RULES)
+    return system
 
 
 def who_won(user, system):
-    result = {user, system}
-
-    if len(result) == 1:
+    selected_input = {user, system}
+    if len(selected_input) == 1:
         return "draw"
     else:
-        return RESULT[tuple(sorted(result))]
+        win_msg = RESULT[tuple(sorted(selected_input))]
+        if win_msg == user:
+            result["user"] += 1
+            print("Yon Win :)")
+        elif win_msg != user:
+            result["system"] += 1
+            print("Yon Lost :(")
+        return win_msg
 
 
 def play(user, system):
-    print(user, system)
+    winner = who_won(user_select, system_select)
+    print(user_select, system_select, winner)
+    print(result["user"], result["system"])
 
 
 user_select = user_select()
 system_select = system_select()
-winner = who_won(user_select, system_select)
-print(user_select, system_select, winner)
+play(user_select, system_select)
